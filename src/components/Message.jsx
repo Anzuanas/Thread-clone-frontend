1728d5bde4b152007ff978ae0cfea793ae7ddfa9
anas <anasginolt@gmail.com>
2025-01-11 23:48:34 +0530

    image sending functionality added to the chat

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,4 +1,4 @@
-import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
+import { Avatar, Box,Image, Flex, Text } from "@chakra-ui/react";
 import { useRecoilValue } from "recoil";
 import userAtom from "../atoms/userAtom";
 import { selectedConversationAtom } from "../atoms/messagesAtom";
@@ -12,21 +12,45 @@ const Message = ({ownMessage, message}) => {
     <>
       {ownMessage ? (
         <Flex gap={2} alignSelf={"flex-end"}>
-          <Flex bg={"green.800"} maxW={"350px"} p={1} borderRadius={"md"}>
+          {message.text &&(
+            <Flex bg={"green.800"} maxW={"350px"} p={1} borderRadius={"md"}>
             <Text color={"white"}>{message.text}</Text>
             <Box alignSelf={"flex-end"} ml={1} color={message.seen ? "blue.400" : ""} fontWeight={"bold"}>
               <BsCheck2All size={16}/>
             </Box>
 
           </Flex>
+          )}
+          {message.img &&(
+            <Flex mt={5} w={"200px"}>
+              <Image
+              src={message.img}
+              alt='Message image'
+              borderRadius={4}
+              />
+            </Flex>
+          )}
           <Avatar src={user.profilePic} w={7} h={7} />
         </Flex>
       ) : (
         <Flex gap={2}>
           <Avatar src={selectedConversation.userProfilePic} w={7} h={7} />
-          <Text maxW={"350px"} bg={"gray.400"} p={1} borderRadius={"md"} color={"black"}>
-            {message.text}
-          </Text>
+          {message.text && (
+             <Text maxW={"350px"} bg={"gray.400"} p={1} borderRadius={"md"} color={"black"}>
+             {message.text}
+           </Text>
+          )}
+
+{message.img &&(
+            <Flex mt={5} w={"200px"}>
+              <Image
+              src={message.img}
+              alt='Message image'
+              borderRadius={4}
+              />
+            </Flex>
+          )}
+         
         </Flex>
       )}
     </>
